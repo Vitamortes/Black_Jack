ad3a3a5fa82f081842a6fd102bc7a3455eb7a3a2
Vitamortes <gamemasterlinus@gmail.com>
2014-12-01 13:53:26 +0100

    une version debuger

--- blackjack.c
+++ blackjack.c
@@ -11,6 +11,10 @@
 *   \version 1.0.0
 */
 
+#define BANQUE 1
+#define JOUEUR 2
+#define BANQUE_CACHEE 3
+
 int main(){
     int choix=0;
     int cartebc;
@@ -18,26 +22,29 @@ int main(){
     int carteJ;
     short scoreb=0;
     short scoreJ=0;
-    
-    cartebc=tirer_carte(BANQUE_cachee);
+
+    cartebc=tirer_carte(BANQUE_CACHEE);
     carteb=tirer_carte(BANQUE);
     afficher_mains(BANQUE);
-    evaluer_score(BANQUE,carteb,scoreb);
-    evaluer_score(BANQUE_cachee,cartebc,scoreb);
-    
+    evaluer_score(BANQUE,carteb,&scoreb);
+    evaluer_score(BANQUE_CACHEE,cartebc,&scoreb);
+
     carteJ=tirer_carte(JOUEUR);
-    evaluer_score(JOUEUR,carteJ,scoreJ);
+    evaluer_score(JOUEUR,carteJ,&scoreJ);
     carteJ=tirer_carte(JOUEUR);
     afficher_mains(JOUEUR);
-    evaluer_score(JOUEUR,carteJ,scoreJ);
+    evaluer_score(JOUEUR,carteJ,&scoreJ);
+
     
-    /*
-     *  while((scoreb<=21)&&(scoreJ<=21)){
-     *      tirer_carte(BANQUE);
-     *      tirer_carte(JOUEUR);
-     *  }
-     */
-    
+    while((scoreb<=21)&&(scoreJ<=21)){
+        if(scoreb<=21){
+            tirer_carte(JOUEUR);
+        }
+        if(scoreJ<=21){
+            tirer_carte(BANQUE);
+        }
+     }
+
     if ((scoreb==21)||(scoreJ>21)){
         printf("La banque a gagné");
         afficher_mains_cachee();
@@ -46,14 +53,14 @@ int main(){
         printf("Le joueur a gané");
         afficher_mains_cachee();
     }
-        
-  
-    /*
+
+
+    
      while(choix!=2){
-        printf("Veuillez choisir si vous desirés une autre carte 1:Oui 2:Non\n");
-        scanf("%",choix);
-        while ((choix==1)&&(choix==2)){
-            scanf("%",choix);
+        printf("Veuillez choisir si vous desirez une autre carte 1:Oui 2:Non\n");
+        scanf("%i",&choix);
+        while ((choix!=1)&&(choix!=2)){
+            scanf("%i",&choix);
         }
         if (choix==1){
             tirer_carte(JOUEUR);
@@ -62,5 +69,6 @@ int main(){
      if(scoreb<=17){
         tirer_carte(BANQUE);
      }
-     */
+    
+     return 0;
 }
