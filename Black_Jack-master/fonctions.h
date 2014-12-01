4ac964325d52ae6f8dfc688abe12b5830286839e
Vitamortes <gamemasterlinus@gmail.com>
2014-12-02 00:25:41 +0100

    Master un black Jack

---
+++ Black_Jack-master/fonctions.h
@@ -0,0 +1,7 @@
+void afficher_mains(short joueur);
+short evaluer_score (short joueur, short carte_recue, short *score);
+void afficher_carte(short num);
+void afficher_mains_cachee();
+short tirer_carte(short joueur);
+short donner_valeur_carte(short joueur, short carte);
+extern short cartes[52];
